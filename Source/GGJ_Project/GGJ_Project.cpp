d3d4d86dc5e1838b6949d4f1814a497b98d56c43
Nevfel Emre <nevfel.cicek@gmail.com>
2023-02-03 18:27:28 +0300

    restart

--- Source/GGJ_Project/GGJ_Project.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GGJ_Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJ_Project, "GGJ_Project" );
